a6365c7be1b4a66f13ea931c86c1dd41d2874f1b
MSLC <MSLC@MSLC.ad>
2019-12-18 12:19:13 +0530

    Added 12/18/2019 lab code

--- sirs/angular/angular-app/projects/app1/src/app/app.module.ts
+++ sirs/angular/angular-app/projects/app1/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Sample } from './sample.component';
 
 import { AppComponent } from './app.component';
+import { XyzComponent } from './xyz/xyz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,Sample, XyzComponent
   ],
   imports: [
     BrowserModule
---
+++ sirs/angular/angular-app/projects/app1/src/app/sample.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core"
+
+@Component({
+   selector: "Sample",
+   template: "<h1> Hello from Sample <h1>"
+
+})
+
+export class Sample {}
