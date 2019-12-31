23519d2def0f51a70997c2150597fa12113edb58
MSLC <MSLC@MSLC.ad>
2019-12-31 12:26:37 +0530

    12-31-2019

---
+++ sirs/angular/angular-app/projects/app1/src/app/xyz/xyz.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-xyz',
+  templateUrl: './xyz.component.html',
+  styleUrls: ['./xyz.component.css']
+})
+export class XyzComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ram = "Angular welcomes you";
+
+  person = {
+       fname: "Ramesh",
+       lname: "Rajamani"
+  };
+
+  users = ["Ramesh", "Ram", "Ravi"];
+
+  sayHello() {
+    alert("Hello");
+  }
+
+}
